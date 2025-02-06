1871730a6ac741e34ad2af07da80ca00dd630e77
eclanin <claninevan@gmail.com>
2025-02-06 14:00:47 -0700

    Create soliltaire.py

---
+++ soliltaire.py
@@ -0,0 +1,19 @@
+#*************************************
+#**  SOLITAIRE: By Evan Clanin      **
+#** 2/6/2025                        **
+#*************************************
+gameRunning=true
+#*************************************
+#** Define constants                **
+#*************************************
+king=11
+queen=12
+jack=13
+
+#*************************************
+#** Variable declaration:           **
+#** Draw stack                      **
+#*************************************
+drawStack_size=52 #(size of stack before being drawn)
+drawStack_suit[52] #(defines suit)
+drawStack_value[52] #(value 1-10/king,queen,jack)
